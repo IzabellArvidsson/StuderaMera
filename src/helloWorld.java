e3d9e17abc7496f21b8f4ea2fc5d8242097c78ab
HannaSoderstrom <57448560+HannaSoderstrom@users.noreply.github.com>
2020-09-08 14:02:18 +0200

    Try again

--- src/helloWorld.java
+++ src/helloWorld.java
@@ -2,6 +2,11 @@
 public class helloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world");
+
+        System.out.println("Hello");
+        System.out.println("World");
+
     }
+
 }
 
