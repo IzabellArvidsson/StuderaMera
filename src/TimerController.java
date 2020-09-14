80b43cdd070135741067646956722288b90daea3
HannaSoderstrom <57448560+HannaSoderstrom@users.noreply.github.com>
2020-09-14 17:16:07 +0200

    Authors: Hanna and Iza. Added a view and a controller for the "are you sure you want to cancel"-part. These can be reused. Implemented the cancel-part so that it kind of works.

--- src/TimerController.java
+++ src/TimerController.java
@@ -9,10 +9,14 @@ import javafx.stage.Stage;
 
 import javax.naming.ldap.Control;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class TimerController extends AnchorPane {
 
     @FXML private AnchorPane timerOnView;
+    @FXML private AnchorPane TimerView;
+    @FXML private AnchorPane popupAnchorPane;
 
     /*public TimerController() {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("TimerView.fxml"));
@@ -33,8 +37,17 @@ public class TimerController extends AnchorPane {
         timerOnView.toFront();
     }
 
-    public void onCLickStopButton () {
-        timerOnView.toBack();
+    public void onCLickStopButton (javafx.scene.input.MouseEvent mouseEvent) throws IOException {
+        /*AnchorPane pane = FXMLLoader.load(getClass().getResource("CancelView.fxml"));
+        popupAnchorPane.getChildren().setAll(pane);
+        popupAnchorPane.toFront();*/
+
+        Parent timerViewParent = FXMLLoader.load(getClass().getResource("CancelView.fxml"));
+        Scene timerViewScene = new Scene(timerViewParent);
+
+        Stage window = (Stage) ((Node) mouseEvent.getSource()).getScene().getWindow();
+        window.setScene(timerViewScene);
+        window.show();
     }
 
     public void onClickGoToHelp (javafx.scene.input.MouseEvent mouseEvent) throws IOException {
