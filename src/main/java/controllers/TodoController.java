c0fe6764367b327b0492b4636ecd8b01c35a7b7c
HannaSoderstrom <57448560+HannaSoderstrom@users.noreply.github.com>
2020-09-24 13:42:11 +0200

    Maven is now working!! Restructured the project after the implementation of maven so that fxml-files and images is in the resources-map outside the java-package.

---
+++ src/main/java/controllers/TodoController.java
@@ -0,0 +1,68 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class TodoController {
+
+    @FXML private AnchorPane addToList;
+    @FXML private AnchorPane toDoListAnchorPane;
+    @FXML private TextField nameTextField;
+
+    String name = "Handlarlista";
+    int task = 3;
+
+    public void onClickBackToOverview(javafx.scene.input.MouseEvent mouseEvent) throws IOException {
+        Parent backToOverviewParent = FXMLLoader.load(getClass().getResource("/fxml_files/PlanOverview.fxml"));
+        Scene backToOverviewScene = new Scene(backToOverviewParent);
+
+        Stage window = (Stage) ((Node) mouseEvent.getSource()).getScene().getWindow();
+        window.setScene(backToOverviewScene);
+        window.show();
+    }
+
+    public void onClickGoToHelp (javafx.scene.input.MouseEvent mouseEvent) throws IOException {
+        Parent toDoParent = FXMLLoader.load(getClass().getResource("/fxml_files/HelpView.fxml"));
+        Scene toDoScene = new Scene(toDoParent);
+
+        Stage window = (Stage) ((Node) mouseEvent.getSource()).getScene().getWindow();
+        window.setScene(toDoScene);
+        window.show();
+    }
+
+    public void openAddToCalendar(){
+        addToList.toFront();
+    }
+
+    public void closeAddToCalendar(){
+        addToList.toBack();
+    }
+
+    @FXML
+    private void onClickStuderaMera (javafx.scene.input.MouseEvent mouseEvent) throws IOException {
+        Parent toDoParent = FXMLLoader.load(getClass().getResource("/fxml_files/firstSideView.fxml"));
+        Scene toDoScene = new Scene(toDoParent);
+
+        Stage window = (Stage) ((Node) mouseEvent.getSource()).getScene().getWindow();
+        window.setScene(toDoScene);
+        window.show();
+    }
+
+    @FXML
+        private void addToDoList(ActionEvent event){
+            //ToDoLists toDoLists = new ToDoLists(name, task);
+            toDoListAnchorPane.getChildren().add(new listInToDoController());
+            closeAddToCalendar();
+        }
+
+
+}
