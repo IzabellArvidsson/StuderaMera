155f6c077ff10c2c86fe2f08b6d7a46781fc7379
Izabell Arvidsson <izaarvidsson@hotmail.com>
2020-10-12 16:08:55 +0200

    Made some changes in FXML-file

--- src/main/java/ViewControllers/CalendarController.java
+++ src/main/java/ViewControllers/CalendarController.java
@@ -3,25 +3,39 @@ package ViewControllers;
 import Factory.IPane;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import java.io.IOException;
+import java.net.URL;
+import java.time.YearMonth;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.ResourceBundle;
 
-public class CalendarController implements IPane  {
+public class CalendarController implements IPane {
 
     @FXML private AnchorPane addToCalendarPane;
+    @FXML Text dateText;
+    @FXML Button nextMonthButton, lastMonthButton;
+
+
 
     private PaneController paneController = new PaneController();
 
-    public void onClickBackToOverview(javafx.scene.input.MouseEvent mouseEvent) throws IOException {
+
+
+    public void onClickBackToOverview() {
         paneController.showOverviewPane();
     }
 
-    public void onClickGoToHelp (javafx.scene.input.MouseEvent mouseEvent) throws IOException {
+    public void onClickGoToHelp () {
         paneController.showHelpPane();
     }
 
@@ -34,7 +48,7 @@ public class CalendarController implements IPane  {
     }
 
     @FXML
-    private void onClickStuderaMera (javafx.scene.input.MouseEvent mouseEvent) throws IOException {
+    private void onClickStuderaMera () {
         paneController.showFirstViewPane();
     }
 
@@ -42,4 +56,6 @@ public class CalendarController implements IPane  {
     public void initPane(PaneController paneController) {
         this.paneController = paneController;
     }
+
+
 }
