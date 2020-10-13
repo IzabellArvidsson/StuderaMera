f7f320c7758057a03e3ab5895205a371ef6f5894
ida <ida.josephine.dahl@gmail.com>
2020-10-13 10:55:55 +0200

    should be able to add calendarEvent
    maybe don't pull is kinda bork

--- src/main/java/Models/CalendarEvent.java
+++ src/main/java/Models/CalendarEvent.java
@@ -5,8 +5,10 @@ import java.util.Calendar;
 
 public class CalendarEvent {
     String name;
-    String hour;
-    String min;
+    String sHour;
+    String sMin;
+    String eHour;
+    String eMin;
     String place;
     String year;
     String month;
@@ -14,10 +16,12 @@ public class CalendarEvent {
     String description;
     Color color;
 
-    public CalendarEvent(String name, String hour, String min, /*String place,*/ String year, String month, String day, /*String description,*/ Color color) {
+    public CalendarEvent(String name, String sHour, String sMin, String eMin, String eHour, /*String place,*/ String year, String month, String day/*, String description,Color color*/ ) {
         this.name = name;
-        this.hour = hour;
-        this.min = min;
+        this.sHour = sHour;
+        this.sMin = sMin;
+        this.eMin = eMin;
+        this.eHour = eHour;
         //this.place = place;
         this.year = year;
         this.month = month;
@@ -26,4 +30,91 @@ public class CalendarEvent {
         this.color = color;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getsHour() {
+        return sHour;
+    }
+
+    public void setsHour(String sHour) {
+        this.sHour = sHour;
+    }
+
+    public String getsMin() {
+        return sMin;
+    }
+
+    public void setsMin(String sMin) {
+        this.sMin = sMin;
+    }
+
+    public String geteHour() {
+        return eHour;
+    }
+
+    public void seteHour(String eHour) {
+        this.eHour = eHour;
+    }
+
+    public String geteMin() {
+        return eMin;
+    }
+
+    public void seteMin(String eMin) {
+        this.eMin = eMin;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getMonth() {
+        return month;
+    }
+
+    public void setMonth(String month) {
+        this.month = month;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public void setDay(String day) {
+        this.day = day;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
 }
--- src/main/java/ViewModels/CalendarViewModel.java
+++ src/main/java/ViewModels/CalendarViewModel.java
@@ -2,6 +2,7 @@ package ViewModels;
 
 import Models.CalendarEvent;
 import Models.ToDoLists;
+import ViewControllers.EventInCalendarController;
 import ViewControllers.listInToDoController;
 import javafx.fxml.FXML;
 import javafx.scene.control.DatePicker;
@@ -18,16 +19,14 @@ public class CalendarViewModel {
 
     private static ArrayList<CalendarEvent> allCalendarEvents = new ArrayList<>();
 
-    public static void addCalendarEvents(String nameTextField, String hour, String min, String year, String month, String day, Color color){
-        CalendarEvent calendarEvents = new CalendarEvent(nameTextField, hour, min, year, month, day, color );
-        // TODO add to GUI
+    public static void addCalendarEvents(String nameTextField, String sHour, String sMin, String eMin, String eHour, String year, String month, String day, /*Color color,*/ FlowPane mondayFlowPane){
+        CalendarEvent calendarEvents = new CalendarEvent(nameTextField, sHour, sMin, eMin, eHour, year, month, day /*, color*/);
+        mondayFlowPane.getChildren().add(new EventInCalendarController(calendarEvents));
         addCalendarEvent(calendarEvents);
         //TODO save list between program startups
         //ToDoListHandler.saveToDoList(allToDoLists);
 
     }
-
-
 
     private static void addCalendarEvent(CalendarEvent calendarEvent){
         allCalendarEvents.add(calendarEvent);
