0edd1ba462cdb6f0374fa43100bf7934ae41a9c1
ida <ida.josephine.dahl@gmail.com>
2020-10-13 17:26:32 +0200

    ok it actually works

--- src/main/java/ViewControllers/CalendarController.java
+++ src/main/java/ViewControllers/CalendarController.java
@@ -1,6 +1,7 @@
 package ViewControllers;
 
 import Factory.IPane;
+import ViewModels.CalendarViewModel;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -9,7 +10,10 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import java.io.IOException;
@@ -22,8 +26,18 @@ import java.util.ResourceBundle;
 public class CalendarController implements IPane {
 
     @FXML private AnchorPane addToCalendarPane;
-    @FXML Text dateText;
+    @FXML TextField dateDay;
+    @FXML TextField dateMonth;
+    @FXML TextField dateYear;
     @FXML Button nextMonthButton, lastMonthButton;
+    @FXML TextField nameTextField;
+    @FXML TextField startTimeHourTextField, startTimeMinTextField, endTimeHourTextField, endTimeMinTextField;
+    @FXML TextField locationTextField;
+    @FXML TextArea descriptionTextArea;
+    @FXML Button addEventButton;
+    @FXML FlowPane mondayFlowPane;
+
+
 
 
 
@@ -56,6 +70,25 @@ public class CalendarController implements IPane {
     public void initPane(PaneController paneController) {
         this.paneController = paneController;
     }
+
+    @FXML
+    private void onClickSaveEvent(){
+        CalendarViewModel.addCalendarEvents(nameTextField.getText(), startTimeHourTextField.getText(), startTimeMinTextField.getText(), endTimeMinTextField.getText(), endTimeHourTextField.getText(), dateYear.getText(), dateMonth.getText(), dateDay.getText(), mondayFlowPane);
+        closeAddToCalendar();
+        nameTextField.clear();
+        dateDay.clear();
+        dateMonth.clear();
+        dateYear.clear();
+        endTimeMinTextField.clear();
+        endTimeHourTextField.clear();
+        startTimeHourTextField.clear();
+        startTimeMinTextField.clear();
+        locationTextField.clear();
+        descriptionTextArea.clear();
+
+    }
+
+
 
 
 }
