bce17c5a81c9771575aaf3407c9a931c3eea9c14
ida <ida.josephine.dahl@gmail.com>
2020-10-14 16:08:13 +0200

    det går att lägga till saker i kalendern i första dagen
    YEEEEEEEEESS

--- src/main/java/ViewModels/CalendarEventHandler.java
+++ src/main/java/ViewModels/CalendarEventHandler.java
@@ -4,25 +4,24 @@ import Models.CalendarEvent;
 import Models.ToDoLists;
 import javafx.scene.layout.FlowPane;
 
-import java.awt.*;
 import java.io.*;
 import java.util.ArrayList;
 
 public class CalendarEventHandler {
 
-    public static void saveCalendarEvent(ArrayList<CalendarEvent> calEvent) {
+    public static void saveToDoList(ArrayList<ToDoLists> toDoLists) {
         try {
-            FileOutputStream fileOut = new FileOutputStream("Models.CalendarEvents.ser");
+            FileOutputStream fileOut = new FileOutputStream("Models.ToDoLists.ser");
             ObjectOutputStream out = new ObjectOutputStream(fileOut);
-            out.writeObject(calEvent);
+            out.writeObject(toDoLists);
             out.close();
             fileOut.close();
-            System.out.print("Serialized data is saved in Models.CalendarEvents.ser");
+            System.out.print("Serialized data is saved in Models.ToDoLists.ser");
         } catch (IOException i) {
             i.printStackTrace();
         }
     }
-
+/*
     public static void writeCalendarEvent(FlowPane flowPane) {
         try {
             FileInputStream fileIn = new FileInputStream("Models.CalendarEvents.ser");
@@ -30,18 +29,8 @@ public class CalendarEventHandler {
             ArrayList<CalendarEvent> calendarEvents = (ArrayList<CalendarEvent>) in.readObject();
             for (int i = 0; i < calendarEvents.size(); i++){
                 String name = calendarEvents.get(i).getName();
-                String day = calendarEvents.get(i).getDay();
-                String sHour = calendarEvents.get(i).getsHour();
-                String sMin = calendarEvents.get(i).getsMin();
-                String eHour = calendarEvents.get(i).geteHour();
-                String eMin = calendarEvents.get(i).geteMin();
-                String place = calendarEvents.get(i).getPlace();
-                String year = calendarEvents.get(i).getYear();
-                String month = calendarEvents.get(i).getMonth();
-                String description = calendarEvents.get(i).getDescription();
-                Color color = calendarEvents.get(i).getColor();
 
-                CalendarViewModel.addCalendarEvents(name, sHour, sMin, eMin, eHour, year, month, day, flowPane);
+                CalendarViewModel.addCalendarEvents(name, flowPane);
             }
             in.close();
             fileIn.close();
@@ -51,7 +40,7 @@ public class CalendarEventHandler {
             System.out.println("Models.CalendarEvents class not found");
             c.printStackTrace();
         }
-    }
+    }*/
 
 }
 
